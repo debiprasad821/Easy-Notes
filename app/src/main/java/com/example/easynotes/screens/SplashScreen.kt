c15fd7affe7926173460bb54f40956c26f2c54c2
debiprasad821 <debiprasaddash821@gmail.com>
2024-09-28 18:30:08 +0530

    setup project and add splash screen

---
+++ app/src/main/java/com/example/easynotes/screens/SplashScreen.kt
@@ -0,0 +1,54 @@
+package com.example.easynotes.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.easynotes.R
+import com.example.easynotes.ui.theme.Teal500
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_notes),
+                contentDescription = null,
+            )
+
+            Text(
+                text = "EASY NOTES",
+                modifier = Modifier
+                    .padding(top = 15.dp),
+                fontSize = 20.sp,
+                fontFamily = FontFamily(Font(R.font.poppins_semi_bold_italic)),
+                color = Teal500,
+                letterSpacing = 2.sp
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(device = Devices.PIXEL)
+fun SplashScreenPreview() {
+    SplashScreen()
+}
